dbe3866c8a37f14174ffc213c781f7ca0b958439
Erdaulet0341 <erdaulet0341@gmail.com>
2023-03-12 14:25:54 +0600

    book store

---
+++ BookStore/app/src/main/java/com/example/bookstore/activities/Sing_In.kt
@@ -0,0 +1,12 @@
+package com.example.bookstore.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bookstore.R
+
+class Sing_In : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sing_in)
+    }
+}
---
+++ BookStore/app/src/main/java/com/example/bookstore/activities/Sing_Up.kt
@@ -0,0 +1,12 @@
+package com.example.bookstore.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bookstore.R
+
+class Sing_Up : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sing_up)
+    }
+}
